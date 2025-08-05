cb9553c747c51524fba9d6c1c4e5b01b072f4e01
Garrett <garrettmjolly@gmail.com>
2025-08-05 14:58:19 -0400

    removed copy_bin.sh (redundant)

--- copy_bin.sh
+++
@@ -1,8 +0,0 @@
-if test -f /usr/local/bin/nyancat; then
-    echo "nyancat binary already exists, removing for fresh build..."
-    sudo rm /usr/local/bin/nyancat
-fi
-
-cargo build --release
-
-sudo cp ./target/release/nyancat /usr/local/bin/
